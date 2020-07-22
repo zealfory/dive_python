c30139e26f72a786a174b95bbb8bc1e2a985df6f
Zoey <shinezzm@foxmail.com>
2020-07-22 14:57:40 +0800

    lc 34

--- lc/34.py
+++ lc/34.py
@@ -4,4 +4,53 @@
 # @Author: ZHANG
 # @Description: 34
 
- 
+
+class Solution:
+    def searchRange(self, nums, target):
+        left = self._bin_search_left(nums, target)
+        right = self._bin_search_right(nums, target)
+        if left < len(nums) and right >= 0 and (nums[left] == nums[right] == target):
+            return [left, right]
+        return [-1, -1]
+
+    def _bin_search_left(self, nums, target):
+        low = 0
+        high = len(nums)
+
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] >= target:
+                high = mid
+            else:
+                low = mid + 1
+
+        return low
+
+    def _bin_search_right(self, nums, target):
+        low = 0
+        high = len(nums)
+
+        while low < high:
+            mid = (low + high) // 2
+            if nums[mid] > target:
+                high = mid
+            else:
+                low = mid + 1
+
+        return high - 1
+
+
+if __name__ == "__main__":
+    nums = [5, 7, 7, 8, 8, 10]
+    target = 8
+    s = Solution()
+    print(s.searchRange(nums, target))
+# [3, 4]
+
+
+    nums = [5, 7, 7, 8, 8, 10]
+    target = 6
+    s = Solution()
+    print(s.searchRange(nums, target))
+# [-1, -1]
+
