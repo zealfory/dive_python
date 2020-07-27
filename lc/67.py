75dfe93405cb675f2768146db8e0574fc40a961b
Zoey <shinezzm@foxmail.com>
2020-07-27 11:58:43 +0800

    update

---
+++ lc/67.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+# @File:   67.py    
+# @Time: 2020-07-27 10:42
+# @Author: ZHANG
+# @Description: 67
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a = int(a, 2)
+        b = int(b, 2)
+        return bin(a+b)[2:]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    a = "1010"
+    b = "1011"
+    print(s.addBinary(a, b))
---
+++ lc/917.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+# @File:   917.py    
+# @Time: 2020-07-27 10:42
+# @Author: ZHANG
+# @Description: 917
+
+
+class Solution:
+    def reverseOnlyLetters(self, S: str) -> str:
+        tmp = list()
+        num_ind = dict()
+        for i in range(len(S)):
+            if S[i].isalpha():
+                tmp.append(S[i])
+            else:
+                num_ind[i] = S[i]
+        s_reversed = list(reversed(tmp))
+        # print(s_reversed)
+        for k, v in num_ind.items():
+            s_reversed.insert(k, v)
+        # print(s_reversed)
+        return ''.join(s_reversed)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.reverseOnlyLetters("ab1c"))
+"""
+S.length <= 100
+33 <= S[i].ASCIIcode <= 122 
+S 中不包含 \ or "
+"""
